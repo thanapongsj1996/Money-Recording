ab28542019ddd29db0aed525710af4541f59a2dc
Thanapong <thanapongsj1996@gmail.com>
2019-05-24 23:42:18 +0700

    add fullbg in login page

---
+++ src/containers/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function LoginPage() {
+  return (
+    <div className="bgLoginPage">
+      
+    </div>
+        );
+      }
+      
+      export default LoginPage;
---
+++ src/containers/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const NotFound = () =>{
+    return (
+        <div>
+   
+            <div className='container col-md-8 text-center'>
+                <h1 className='mt-5' style={{fontSize: 120}}>404</h1>
+                <h2 className='mb-4'>Not Found</h2>
+                <p className='title mb-5'>ขออภัยไม่พบหน้าที่คุณค้นหา ดูเหมือนว่าเว็บที่คุณพยายามเข้าถึงไม่มีอยู่อีกต่อไปหรืออาจจะย้ายไปยังหน้าเว็บเพจอื่น</p>
+            </div>
+     
+        </div>
+    )
+}
+
+export default NotFound
